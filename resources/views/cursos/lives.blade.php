bd2f6157e50e82045d9eb18c7f0ca46e721f4dc5
Iruhary <iruhary@gmail.com>
2021-08-24 19:48:49 -0500

    Se suben cambios vista live

--- resources/views/cursos/lives.blade.php
+++ resources/views/cursos/lives.blade.php
@@ -21,21 +21,60 @@
             </div>
         </div>
     </div>
-        <div class="row">
-            <div class="container mt-4" id="divCurso">
-                <div class="row justify-content-md-start justify-content-center">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/4WHmvhQL6AU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div class="row col-12 justify-content-center row-category-videos">
+        <div class="h-100 align-items-center d-lg-inline-flex col-12">
+            <div class="col-12 col-sm-12 text-left "> </br>   
+                <div class="container" id="divCurso">
+                    <div class="row justify-content-md-start justify-content-center">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4WHmvhQL6AU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
         <div class="row">
             <div class="container mt-4">
-                <div class="row justify-content-md-start justify-content-center">
-                        <label for="fechaInicial" class="col-md-4 col-form-label text-md-right title-small label-form">Filtro de fechas</label>
-                            <div class="col-md-6">
+                <div class="row justify-content-md-start justify-content-center mt-3">
+                    <div class="h-100 align-items-center d-lg-inline-flex col-12 justify-content-center">
+                                <h1 style="color: #70b62c !important; font-weight: bold; ">Lista de próximos lives</h1></br>
+                           
+                    </div>
+                </div></br>
+                <div class="row justify-content-md-start justify-content-center mt-3">
+                        <label for="fechaInicial" class="col-md-3 col-form-label text-md-right title-small label-form">Filtro de fechas</label></br>
+                        <div class="col-md-4">
                                 <input type="text" class="form-control custom-input" placeholder="Fecha inicial" name="fechaInicial" id="fechaInicial">
+                        </div>
+                        <div class="col-md-4">
                                 <input type="text" class="form-control custom-input" placeholder="Fecha final" name="fechaFinal" id="fechaFinal">
-                            </div>
+                        </div>
+                </div></br>
+                <div class="row justify-content-md-start justify-content-center mt-4">
+                <table class="table table-sm" style="color: #48448f !important;">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nombre del live</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Fecha y Hora</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -45,18 +84,57 @@
                 <div class="container h-100" > 
                     <div class="h-100 align-items-center d-lg-inline-flex col-12">
                             <div class="col-12 col-sm-6 text-left ">
-                                <h1 style="color: #48448f !important; font-weight: bold;">Próximos cursos</h1>
+                                <h1 style="color: #48448f !important; font-weight: bold;">Próximos cursos</h1></br>
                             </div>
-                            <div class="card" style="width: 18rem;">
-                                <img src="..." class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                            </div>
+                        </div>
+                    </div>
+                    <div class="card ml-4" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary" style="background-color:#70b62c;">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card ml-4" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary" style="background-color:#70b62c;">Go somewhere</a>
+                        </div>
+                    </div>
+                    <div class="card ml-4" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary" style="background-color:#70b62c;">Go somewhere</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="container content-f1 mt-5">
+                <div class="row mrl-q">
+                <div class="container h-100">
+                    <div class="align-items-center d-lg-inline-flex col-12">
+                            <div class="col-12 col-sm-6 text-left ">
+                                <h1 style="color: #70b62c; font-weight: bold;">¡CONOCE LOS </h1><h1 style="color: #48448f !important; font-weight: bold;">E-BOOKS</h1><h1 style="color: #70b62c; font-weight: bold;"> DE TRADING!</h1>
+                                <p class="lead">Conoce el material extra que tenemos para ti...</p>
+                            </div>
+                            <div class="d-lg-inline col-sm-12 col-md-2 col-12 text-lg-left text-center">
+                                <a href="{{ asset('public/doc_1.pdf') }}" download="Manual de Trading Avanzado"><img src="{{ asset('public/img/Libros/DF-book.jpg') }}" class="img-curses"></a>
+                            </div>
+                            <div class="d-lg-inline col-sm-12 col-md-2 col-12 text-lg-left text-center ">
+                                <a href="{{ asset('public/doc_2.pdf') }}" download="Curso Intensivo Trading"><img src="{{ asset('public/img/Libros/DF-book002.jpg') }}" class="img-curses"></a>
+                            </div>
+                            <div class="d-lg-inline col-sm-12 col-md-2 col-12 text-lg-left text-center ">
+                                <img src="{{ asset('public/img/Libros/DF-book003.jpg') }}" class="img-curses">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </section>
 @endsection
